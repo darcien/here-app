962bd18fe021b06344560657150ecbf929744d44
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2018-09-21 14:28:26 +0700

    Add initial route.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+// @flow
+import {createBottomTabNavigator} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import FeedScene from './components/scenes/FeedScene/FeedScene';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createBottomTabNavigator(
+  {
+    Feed: {
+      screen: FeedScene,
+    },
   },
-});
+  {
+    initialRouteName: 'Feed',
+  },
+);
